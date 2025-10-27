b1a067dc88c982191f5f67fa1a15dcc9df72648e
AlexanderGutHiller <99763799+AlexanderGutHiller@users.noreply.github.com>
2025-10-27 09:18:51 +0100

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {'Hello': 'World'}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def root():
+    return {"message": "Smoobu Staff Planner ready"}
+
+@app.get("/admin/132576781", response_class=HTMLResponse)
+async def admin_home(request: Request):
+    return templates.TemplateResponse("admin_home.html", {"request": request})
